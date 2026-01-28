234d76c40dded97059919cae3a9f91abdf0cf83d
vhsotti <vhsotti@hotmail.com>
2026-01-28 10:48:04 -0300

    Products

--- src/main/java/com/educandoweb/course/entities/Category.java
+++ src/main/java/com/educandoweb/course/entities/Category.java
@@ -2,17 +2,23 @@ package com.educandoweb.course.entities;
 
 import jakarta.persistence.*;
 
+import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_category")
-public class Category {
+public class Category implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @Transient
+    private Set<Product> products = new HashSet<>();
 
     public Category(){
     }
@@ -38,6 +44,10 @@ public class Category {
         this.name = name;
     }
 
+    public Set<Product> getProducts() {
+        return products;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
@@ -49,4 +59,5 @@ public class Category {
     public int hashCode() {
         return Objects.hashCode(getId());
     }
+
 }
